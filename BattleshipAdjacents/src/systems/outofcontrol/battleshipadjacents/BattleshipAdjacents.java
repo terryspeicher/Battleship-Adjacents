21cc69de77b5a794374942a9822a536abf29a6c0
TBone <dnr@tspeicher.com>
2017-09-01 07:16:12 -0600

    Created project, package, and first Java class file in Eclipse.
    Now pushing up the newly created files.

---
+++ BattleshipAdjacents/src/systems/outofcontrol/battleshipadjacents/BattleshipAdjacents.java
@@ -0,0 +1,17 @@
+package systems.outofcontrol.battleshipadjacents;
+
+import java.applet.Applet;
+
+public class BattleshipAdjacents extends Applet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7678032691516184112L;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
